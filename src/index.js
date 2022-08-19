9234c6b2c9d715682f5ac53da16eb1a2941ee120
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2022-08-19 12:43:46 -0400

    create function to process json data into object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-//get weather data from API
-
 const btn = document.querySelector('button');
 const form = document.querySelector('.search-form');
 const search = document.querySelector('#search');
@@ -7,20 +5,37 @@ const search = document.querySelector('#search');
 btn.addEventListener('click', weatherAPI);
 form.addEventListener('submit', weatherAPI);
 
+//process JSON data
+
+function convertData(data) {
+        const {
+            name: city,
+            main: {temp: temperature, feels_like: feelsLike, humidity, temp_min: minTemp, temp_max: maxTemp},
+            weather: {description: conditions, icon: icon, main: main},
+            sys: {country: countryCode},
+        } = data;
+    return {city, temperature, feelsLike, humidity, minTemp, maxTemp, conditions, icon, countryCode, main};
+}
+
+//get weather data from API 
+
 async function weatherAPI(e) {
     let searchValue = search.value;
 
     if(!searchValue) {
-        console.log('Must enter a city name');
+        alert('Must enter a city name');
     }
     e.preventDefault();
 
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=4af338f9a4a4bfc6ef0dfbe08ae0fe35&units=metric`, {mode: 'cors'});
-        const weatherData = await response.json();
+        if(!response.ok) throw new Error(`${searchValue} is not a valid city`);
+        const weatherData = convertData(await response.json());
         console.log(weatherData);
     }
     catch(err) {
         console.log(err);
+        return null;
     };
-};
+}
+return {weatherAPI};
