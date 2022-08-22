03e8fc3ada7d90933a7e776975091142f345edf9
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2022-08-22 11:37:23 -0400

    display weather information on page

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-(()=>{const e=document.querySelector("button"),t=document.querySelector(".search-form"),n=document.querySelector("#search");async function o(e){let t=n.value;t||alert("Must enter a city name"),e.preventDefault();try{const e=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${t}&appid=4af338f9a4a4bfc6ef0dfbe08ae0fe35&units=metric`,{mode:"cors"});if(!e.ok)throw new Error(`${t} is not a valid city`);const n=function(e){const{name:t,main:{temp:n,feels_like:o,humidity:a,temp_min:i,temp_max:c},weather:{description:r,icon:m,main:s},sys:{country:u}}=e;return{city:t,temperature:n,feelsLike:o,humidity:a,minTemp:i,maxTemp:c,conditions:r,icon:m,countryCode:u,main:s}}(await e.json());console.log(n)}catch(e){return console.log(e),null}}e.addEventListener("click",o),t.addEventListener("submit",o)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const btn = document.querySelector('button');\nconst form = document.querySelector('.search-form');\nconst search = document.querySelector('#search');\n\nconst cityName = document.querySelector('.city-name');\nconst mainTemp = document.querySelector('.main-temp');\nconst feelsLikeTemp = document.querySelector('.feels-like');\nconst cityHumidity = document.querySelector('.humidity');\nconst icon = document.querySelector('img');\nconst minTemperature = document.querySelector('.min-temp');\nconst maxTemperature = document.querySelector('.max-temp');\nconst weatherCondition = document.querySelector('.weather-condition');\nconst visibility = document.querySelector('.visibility');\nconst cloudiness = document.querySelector('.cloudiness');\n\nbtn.addEventListener('click', weatherAPI);\nform.addEventListener('submit', weatherAPI);\n\n//get weather data from API \n\nasync function weatherAPI(e) {\n    let searchValue = search.value;\n\n    if(!searchValue) {\n        alert('Must enter a city name');\n    }\n    e.preventDefault();\n\n    try {\n        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=4af338f9a4a4bfc6ef0dfbe08ae0fe35&units=metric`, {mode: 'cors'});\n        if(!response.ok) throw new Error(`${searchValue} is not a valid city`);\n        const weatherData = await response.json();\n        console.log(weatherData);\n        displayWeather(weatherData);\n\n    }\n    catch(err) {\n        console.log(err);\n        return null;\n    };\n    return weatherAPI;\n}\n\n//display weather data\n\nfunction displayWeather(weatherData) {\n        icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;\n        cityName.innerHTML = `${weatherData.name}, ${weatherData.sys.country}`;\n        mainTemp.innerHTML = `Temperature: ${(weatherData.main.temp).toFixed(1)} °C`; \n        weatherCondition.innerHTML = `${(weatherData.weather[0].description).toUpperCase()}`;\n        feelsLikeTemp.innerHTML = `Feels like: ${(weatherData.main.feels_like).toFixed(1)} °C`;\n        cityHumidity.innerHTML = `Humidity index: ${weatherData.main.humidity} %`;\n        minTemperature.innerHTML = `Min: ${(weatherData.main.temp_min).toFixed(1)} °C`;\n        maxTemperature.innerHTML = `Max: ${(weatherData.main.temp_max).toFixed(1)} °C`;\n        visibility.innerHTML = `Visibility: ${(weatherData.visibility/1000).toFixed(1)} km`;\n        cloudiness.innerHTML = `Cloudiness: ${(weatherData.clouds.all).toFixed(1)} %`;\n}\n\n// change display background\n\n\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ const btn = document.querySelector('button');
 const form = document.querySelector('.search-form');
 const search = document.querySelector('#search');
 
+const cityName = document.querySelector('.city-name');
+const mainTemp = document.querySelector('.main-temp');
+const feelsLikeTemp = document.querySelector('.feels-like');
+const cityHumidity = document.querySelector('.humidity');
+const icon = document.querySelector('img');
+const minTemperature = document.querySelector('.min-temp');
+const maxTemperature = document.querySelector('.max-temp');
+const weatherCondition = document.querySelector('.weather-condition');
+const visibility = document.querySelector('.visibility');
+const cloudiness = document.querySelector('.cloudiness');
+
 btn.addEventListener('click', weatherAPI);
 form.addEventListener('submit', weatherAPI);
-
-//process JSON data
-
-function convertData(data) {
-        const {
-            name: city,
-            main: {temp: temperature, feels_like: feelsLike, humidity, temp_min: minTemp, temp_max: maxTemp},
-            weather: {description: conditions, icon: icon, main: main},
-            sys: {country: countryCode},
-        } = data;
-    return {city, temperature, feelsLike, humidity, minTemp, maxTemp, conditions, icon, countryCode, main};
-}
 
 //get weather data from API 
 
@@ -30,12 +29,33 @@ async function weatherAPI(e) {
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=4af338f9a4a4bfc6ef0dfbe08ae0fe35&units=metric`, {mode: 'cors'});
         if(!response.ok) throw new Error(`${searchValue} is not a valid city`);
-        const weatherData = convertData(await response.json());
+        const weatherData = await response.json();
         console.log(weatherData);
+        displayWeather(weatherData);
+
     }
     catch(err) {
         console.log(err);
         return null;
     };
+    return weatherAPI;
 }
-return {weatherAPI};
+
+//display weather data
+
+function displayWeather(weatherData) {
+        icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+        cityName.innerHTML = `${weatherData.name}, ${weatherData.sys.country}`;
+        mainTemp.innerHTML = `Temperature: ${(weatherData.main.temp).toFixed(1)} °C`; 
+        weatherCondition.innerHTML = `${(weatherData.weather[0].description).toUpperCase()}`;
+        feelsLikeTemp.innerHTML = `Feels like: ${(weatherData.main.feels_like).toFixed(1)} °C`;
+        cityHumidity.innerHTML = `Humidity index: ${weatherData.main.humidity} %`;
+        minTemperature.innerHTML = `Min: ${(weatherData.main.temp_min).toFixed(1)} °C`;
+        maxTemperature.innerHTML = `Max: ${(weatherData.main.temp_max).toFixed(1)} °C`;
+        visibility.innerHTML = `Visibility: ${(weatherData.visibility/1000).toFixed(1)} km`;
+        cloudiness.innerHTML = `Cloudiness: ${(weatherData.clouds.all).toFixed(1)} %`;
+}
+
+// change display background
+
+
